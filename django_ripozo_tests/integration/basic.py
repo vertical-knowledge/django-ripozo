04084bfd917bec269673061f931ac7f2c550da74
timmartin19 <tim.martin@vertical-knowledge.com>
2015-04-12 23:06:50 -0400

    Fixed python 3.4 tests.

--- django_ripozo_tests/integration/basic.py
+++ django_ripozo_tests/integration/basic.py
@@ -8,6 +8,7 @@ from django.test import Client
 from django_ripozo_tests.helpers.common import UnittestBase
 
 import json
+import six
 import unittest
 
 
@@ -19,11 +20,11 @@ class TestBasic(UnittestBase, unittest.TestCase):
     def test_list_url(self):
         response = self.client.get('/api/myresource/')
         self.assertEqual(response.status_code, 200)
-        data = json.loads(response.content)
+        data = json.loads(six.text_type(response.content))
         self.assertDictEqual(data['properties'], dict(hello='world'))
 
     def test_individual_url(self):
         response = self.client.get('/api/myidresource/1/')
         self.assertEqual(response.status_code, 200)
-        data = json.loads(response.content)
+        data = json.loads(six.text_type(response.content))
         self.assertDictEqual(data['properties'], dict(hello='world', id='1'))
