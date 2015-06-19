50f5164e083a87c6ab19adc390b8096896922604
timmartin19 <tim.martin@vertical-knowledge.com>
2015-06-18 20:59:33 -0400

    Removed shit from app.

--- test_project/settings.py
+++ test_project/settings.py
@@ -31,24 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
     'django_ripozo',
     'testapp'
 )
 
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+MIDDLEWARE_CLASSES = tuple()
 
 ROOT_URLCONF = 'testapp.urls'
 
